717c511268434d83872927459d97d8613ff76a46
Android32018 <Androidhcl@123>
2019-04-04 11:05:37 +0530

    pressive commit

--- src/srp/Modem.java
+++ src/srp/Modem.java
@@ -1,5 +1,23 @@
 package srp;
 
 public interface Modem {
+	public void dial(String phone);
+	public void hangup();
+	
+	public void send(char ch);
+	public char receive();
+}
+/*
+ * This interface got two responsibility, one is connection management 
+ * and the other is data communication
+ * We can make 2 interfaces here to achieve SRP
+*/
+interface ModemConnection{
+	public void dial(String phone);
+	public void hangup();
+}
 
+interface ModemDataManagement{
+	public void send(char ch);
+	public char receive();
 }
